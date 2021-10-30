2a989044c7d5a10ee513c2d9e17c32edf8bbd4cb
Nazmul Hosen <nazmulhosenm668@gmail.com>
2021-10-31 01:24:40 +0600

    all setup done

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ async function run() {
 		const database = client.db("dark-thunder");
 		const ridesCollection = database.collection("rides");
 		const orderRides = database.collection("orders");
+		const purchaseCollection = database.collection("purchases");
 
 		// GET DATA
 		app.get("/rides", async (req, res) => {
@@ -67,10 +68,67 @@ async function run() {
 			res.json(result);
 		});
 
-		// GET all order ride
+		// GET all order for manage orders
 		app.get("/orders", async (req, res) => {
 			const result = await orderRides.find({}).toArray();
 			res.send(result);
+		});
+		// Delete from manage order
+		app.delete("/orders/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: ObjectId(id) };
+			const result = await orderRides.deleteOne(query);
+
+			res.json(result);
+		});
+
+		// Post purchase data from myOrders
+		app.post("/purchase", async (req, res) => {
+			const purchaseData = req.body;
+			const result = await purchaseCollection.insertOne(purchaseData);
+			res.json(result);
+		});
+
+		// get status by email
+		app.get("/purchase/:email", async (req, res) => {
+			const email = req.params.email;
+			const result = await purchaseCollection
+				.find({ email: email })
+				.toArray();
+			res.send(result);
+		});
+
+		// Get puchase data
+		app.get("/purchase", async (req, res) => {
+			const result = await purchaseCollection.find({}).toArray();
+			res.send(result);
+		});
+
+		// update purchase status
+		app.put("/status/:email", async (req, res) => {
+			const userEmail = req.params.email;
+			const updatedStatus = req.body;
+			const filter = { email: userEmail };
+			const options = { upsert: true };
+			const updateDoc = {
+				$set: {
+					status: updatedStatus.status,
+				},
+			};
+			const result = await purchaseCollection.updateOne(
+				filter,
+				updateDoc,
+				options
+			);
+			res.json(result);
+		});
+
+		// purchase delete
+		app.delete("/purchase/:email", async (req, res) => {
+			const email = req.params.email;
+			const result = await purchaseCollection.deleteOne({ email: email });
+			res.json(result);
+			console.log("purchase delete hitting", result);
 		});
 	} finally {
 		// await client.close();
